b24062f3553c5a5c5e16737ea07ad857cb0e7560
Willian Goncalves Rios <williangrios@yahoo.com.br>
2023-04-19 07:35:01 -0300

    update

--- backend/routes/partyRoutes.js
+++ backend/routes/partyRoutes.js
@@ -107,9 +107,85 @@ router.get("/:id", async (req, res) => {
     try {
         const id = req.params.id;
         const party = await Party.findOne({_id: id});
+
+        if (!party.privacy){
+            res.json({error: null, data: party})
+        }else{
+            const token = req.header("auth-token");
+            const userByToken = await getUserByToken(token);
+            const userId = userByToken._id.toString();
+            const userPartyId = party.userId.toString();
+            if(userId === userPartyId){
+                res.json({error: null, data: party})
+            }
+        }
     } catch (error) {
         return res.status(400).json({error: error});
     }
+})
+
+
+//delete events
+router.delete("/", verifyToken,  async (req, res) => {
+    const token = req.header("auth-token");
+    const userByToken = await getUserByToken(token);
+    const userId = userByToken._id.toString();
+    const partyId = party._id.toString();
+    try {
+        await Party.deleteOne({_id: partyId, userId: userId})
+        res.json({error: null, data: "Deleted"})
+    } catch (error) {
+        return res.status(400).json({error: error});
+    }
+})
+
+
+//update
+router.put("/", verifyToken, async (req, res) => {
+    const {title, description, partyDate, id: partyId, userId: partyUserId, privacy} = req.body;
+
+    let files = [];
+    if (req.files){
+        files = req.files.photos;
+    }
+
+    if(title === null || description === null || partyDate === null){
+        return res.status(401).json({error: "Fill all the fields"})
+    }
+
+    const token = req.header("auth-token");
+    const userByToken = await getUserByToken(token);
+    const userId = userByToken._id.toString();
+    
+    if (userId != partyUserId){
+        return res.status(401).json({error: "Not allowed"})
+    }
+
+    const party = {
+        id: partyId,
+        title,
+        description, 
+        partyDate,
+        privacy,
+        userId
+    };
+
+    let photos = [];
+    if(files && files.length > 0 ){
+        files.forEach((pht, i) => {
+            photos[i] = pth.path;
+        });
+
+        party.photos = photos;
+    }
+
+    try {
+        const updatedParty = await Party.findOneAndUpdate({_id: partyId, userId: userId}, {$set: party}, {new: true});
+        res.json({error: null, msg:"Updated", data: updatedParty});
+    } catch (error) {
+        return res.status(400).json({error: error});
+    }
+
 })
 
 module.exports = router;
